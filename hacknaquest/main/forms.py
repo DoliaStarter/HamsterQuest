a2b7225c0dfb9af3f4934b1935b05523319f8262
Bohdan Forostianyi <qwebeck5@gmail.com>
2020-03-07 23:52:12 +0100

    Implementd basic form

--- hacknaquest/main/forms.py
+++ hacknaquest/main/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    name = forms.CharField(label="Your name",
+    name = forms.CharField(label="Name",
                            max_length=100)
+    login = forms.CharField(label="Surname",
+                            max_length=100)
+    passwd = forms.CharField(widget=forms.PasswordInput)
--- hacknaquest/main/static/scripts/sm_script.js
+++ hacknaquest/main/static/scripts/sm_script.js
@@ -1,22 +1,29 @@
+// Cool, but you can make it more generic !
+// Method, that will switch between tabs.
 function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  } 
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-  document.getElementById("defaultOpen").click();
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+ 
+  // for (tab of tablinks)
+  // tab.className = tab.className.replace(" active", "")
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// More readale form
+// openTab(null,"Task")
+document.getElementById("defaultOpen").click();
