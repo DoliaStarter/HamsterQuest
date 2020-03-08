01f5a2e0efecb3857fc7244ce3f8018fe0e19d97
vvlovsky <slvtrshkv@gmail.com>
2020-03-08 23:22:44 +0100

    1.added roboto-font 2.implemented achievements 3.etc

---
+++ hacknaquest/main/static/scripts/user_cabinet/index.js
@@ -0,0 +1,29 @@
+let uc_variables = {
+    nickname: "kl@dmen2007",
+    profile_image_src: "/static/images/profile_image.jpg",
+    user_achievements: "11010",
+    creator_achievements: "01000"
+};
+
+function uc_init() {
+    let profile_image = document.getElementById("profile_image");
+    profile_image.src = uc_variables.profile_image_src;
+    console.log("hi");
+    let userAchievements = document.getElementById("player_achievements").childNodes;
+    for (let i = 1; i < userAchievements.length; i += 2) {
+        if (uc_variables.user_achievements[(i+1)/2-1] === "1") {
+            userAchievements[i].classList.remove('not_completed');
+            userAchievements[i].classList.add('completed');
+        }
+    }
+    let creatorAchievements = document.getElementById("creator_achievements").childNodes;
+    for (let i = 1; i < userAchievements.length; i += 2) {
+        if (uc_variables.creator_achievements[(i + 1) / 2 - 1] === "1") {
+            creatorAchievements[i].classList.remove('not_completed');
+            creatorAchievements[i].classList.add('completed');
+        }
+    }
+
+}
+
+export {uc_init}
