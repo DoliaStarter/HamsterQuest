511f2dc9668fb302c0de3b2c5f69920fa4967ad7
vvlovsky <slvtrshkv@gmail.com>
2020-03-16 03:08:02 +0100

    search_quests basic implementation

--- hacknaquest/user_cabinet/urls.py
+++ hacknaquest/user_cabinet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('user_cabinet/', views.index, name='index'),
+    path('search_quests/', views.search_quests, name='search_quests')
 ]
