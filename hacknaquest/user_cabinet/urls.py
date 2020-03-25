3a504f1a7c657f432bca97a51324eb22162b98a8
vvlovsky <slvtrshkv@gmail.com>
2020-03-25 01:43:02 +0100

    small paths fixes

--- hacknaquest/user_cabinet/urls.py
+++ hacknaquest/user_cabinet/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user_cabinet/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('search_quests/', views.search_quests, name='search_quests')
 ]
